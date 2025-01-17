0e6255573e57e1edb892c3e035ff53534e339816
rodhfr <souzafrodolfo@gmail.com>
2025-01-17 15:02:04 -0300

    add systemd folder and wayvnc

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 cp -rf ./.config/yt-dlp/ ~/.config/
 cp -rf ./.config/mpv/ ~/.config/
+cp -rf ./.config/wayvnc/ ~/.config/
+cp -rf ./.config/systemd/ ~/.config/
 cp -rf ./.config/zshrc/* ~/dotfiles/.config/zshrc/
 cp -rf ./.config/hypr/* ~/dotfiles/.config/hypr/
 
